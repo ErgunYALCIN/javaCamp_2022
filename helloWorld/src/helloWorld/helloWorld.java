7aa65bd89b34f469d4195a81459462df016a086c
Ergun YALÇIN <113599859+ErgunYALCIN@users.noreply.github.com>
2022-09-20 12:39:49 +0300

    Add files via upload

---
+++ helloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System
+		.out
+		.println("Hello Java");
+
+	}
+
+}
---
+++ miniProje2/src/miniProje2/sesliHarfler.java
@@ -0,0 +1,22 @@
+package miniProje2;
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'I';
+		
+		switch(harf) {
+		 	case 'A':
+		 	case 'O':
+		 	case 'I':
+		 	case 'U':
+		 		System.out.println("Kalın Sesli Harf");
+		 		break;
+		 		default:
+		 			System.out.println("İnce Sesli Harf");
+		}
+		
+
+	}
+
+}
