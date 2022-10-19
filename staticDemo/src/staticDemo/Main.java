fd8972b520eaa1968d52ae19db011b2d4709942f
Ergun YALÇIN <113599859+ErgunYALCIN@users.noreply.github.com>
2022-10-19 09:31:35 +0300

    Add files via upload

---
+++ staticDemo/src/staticDemo/Main.java
@@ -0,0 +1,20 @@
+package staticDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ProductManager manager = new ProductManager();
+		Product product = new Product();
+		
+		product.price = 10;
+		product.name = "";
+		product.id = 1;
+		
+		
+		manager.add(product);	
+		
+
+	
+	}
+
+}
